98523424a044fc62d61578d2797b58c3c21c21a2
Aleksey <makanprogram@gmail.com>
2025-12-06 20:45:39 +0100

    новые изменения

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+const loginElem = document.querySelector(`.login-form`)
+
+loginElem.addEventListener('submit', event => {
+    event.preventDefault(); 
+
+    const { email, password } = loginElem.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+    }
+
+    const formData = {
+    email: emailValue,
+    password: passwordValue,
+    };
+
+    console.log(formData); 
+
+    loginElem.reset(); 
+});
